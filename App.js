f531f0e6e38a5364e96f4e06818e50dcd7a8c962
helgakern <helgakern@gmail.com>
2020-06-11 13:25:01 -0700

    setup react navigation stack

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import Header from './Header';
 import PokemonList from './PokemonList';
 
 const POKEMON_API_ENDPOINT = `https://pokeapi.co/api/v2/`;
 const FIRST_100_QUERY = `pokemon?limit=100`;
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const [pokemonList, setPokemonList] = useState([]);
@@ -20,12 +24,25 @@ export default function App() {
   }, [])
 
   return (
-    <View style={styles.container}>
+    <NavigationContainer>
       <Header/>
       <View style={styles.body}>
-        <PokemonList list={pokemonList}/>
+        <Stack.Navigator
+          screenOptions={
+            {
+              headerShown: false
+            }
+          }
+        >
+          <Stack.Screen
+            name="Pokemon List"
+            children={() => {
+              return <PokemonList list={pokemonList}/>
+            }}
+          />
+        </Stack.Navigator>
       </View>
-    </View>
+    </NavigationContainer>
   );
 }
 
