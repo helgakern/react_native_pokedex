a4f9f83233879a938aba4d70594b27f80f33e778
helgakern <helgakern@gmail.com>
2021-04-01 16:11:36 -0700

    yarn upgrade

--- PokemonDetails.js
+++ PokemonDetails.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet, Button } from 'react-native';
 
+const DIRECTION_LEFT = 'left'
+const DIRECTION_RIGHT = 'right'
 function PokemonDetails(props) {
   const { route } = props;
   const { params } = route;
 
   const [pokemon, setPokemon] = useState({});
+  const [currentSprite, setCurrentSprite] = useState(0);
 
   useEffect(() => {
     fetch(params.pokemon.url)
@@ -15,17 +18,38 @@ function PokemonDetails(props) {
       })
   }, [])
 
+  function updateDisplaySprite(direction) {
+    if (direction === DIRECTION_LEFT) {
+      if (currentSprite > 0) {
+        setCurrentSprite(currentSprite - 1)
+      } else {
+        setCurrentSprite(Object.keys(pokemon.sprites).length - 1)
+      }
+    } else {
+      if (currentSprite === Object.keys(pokemon.sprites).length - 1) {
+        setCurrentSprite(0)
+      } else {
+        setCurrentSprite(currentSprite + 1)
+      }
+    }
+  }
+
+  const content = pokemon.name ? (
+    <>
+      <View style={styles.imageContainer}>
+        <Button style={styles.btn} title='👈' onPress={ () => updateDisplaySprite(DIRECTION_LEFT) }/>
+        <Image style={styles.image} source={{ uri: pokemon.sprites[Object.keys(pokemon.sprites)[currentSprite]] || '#' }}/>
+        <Button style={styles.btn} title='👉' onPress={ () => updateDisplaySprite(DIRECTION_RIGHT) }/>
+      </View>
+      <Text style={styles.h1}>{pokemon.name}</Text>
+      <Text style={styles.text}>Weight: {pokemon.weight}</Text>
+    </>
+    ) :
+    <Text>Loading...</Text>
+
   return(
     <View style={styles.container}>
-      { pokemon.name ? (
-        <>
-          <Image style={styles.image} source={{ uri: pokemon.sprites.front_default}}/>
-          <Text style={styles.h1}>{pokemon.name}</Text>
-          <Text style={styles.text}>Weight: {pokemon.weight}</Text>
-        </>
-        ) :
-        <Text>Loading...</Text>
-      }
+      { content }
     </View>
   )
 }
@@ -35,12 +59,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center'
   },
+  imageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
   image: {
     height: 300,
-    width: 300
+    width: 300,
   },
   h1: {
-    fontSize: 50
+    fontSize: 50,
   },
   text: {
     fontSize: 25
